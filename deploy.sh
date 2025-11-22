5f3f210f1ee7fdd5e73cc4fdaeee0f58463e61d3
MUHAMMAD ARFAN <106101890+arfan4-dev@users.noreply.github.com>
2025-11-22 19:54:28 +0500

    Modify npm install to ignore scripts during deployment
    Added --ignore-scripts option to npm install command.

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,13 @@ set -e
 echo "====== DEPLOYMENT STARTED ======"
 
 echo ">>> Pulling latest code from master branch..."
-# Yahan 'git' ka poora path likhein
 /usr/bin/git pull origin master
 
 echo ">>> Installing NPM dependencies..."
-# Yahan 'npm' ka poora path likhein
-/usr/bin/npm install --production
+# Yahan --ignore-scripts add kiya hai
+/usr/bin/npm install --production --ignore-scripts
 
 echo ">>> Restarting application with PM2..."
-# Yahan 'pm2' ka poora path likhein
 /usr/bin/pm2 restart irapp-be --update-env
 
 echo "====== DEPLOYMENT COMPLETED SUCCESSFULLY ======"
